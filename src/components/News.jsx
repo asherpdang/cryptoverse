64f7f205a711a5aa5ce5cbdb33965be5d863d57b
Asher Dang <asherpdang@gmail.com>
2022-05-25 21:37:09 -0700

    Fix Navbar, Homepage, News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -15,15 +15,13 @@ const News = ({ simplified }) => {
   const { data } = useGetCryptosQuery(100);
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
 
-  console.log(cryptoNews);
-
-  if(!cryptoNews?.value) return 'Loading...';
+  if (!cryptoNews?.value) return 'Loading...';
 
   return (
     <Row gutter={[24, 24]}>
       {!simplified && (
         <Col span={24}>
-          <Select 
+          <Select
             showSearch
             className='select-news'
             placeholder="Select a crypto"
@@ -37,17 +35,17 @@ const News = ({ simplified }) => {
         </Col>
       )}
       {cryptoNews.value.map((news, i) => (
-        <Col 
-          xs={24} 
-          sm={12} 
-          lg={8} 
+        <Col
+          xs={24}
+          sm={12}
+          lg={8}
           key={i}
         >
           <Card hoverable className='news-card'>
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{news.name}</Title>
-                <img style={{ maxWidth: '200px', maxHeight: '200px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt='news'/>
+                <img style={{ maxWidth: '200px', maxHeight: '200px' }} src={news?.image?.thumbnail?.contentUrl || demoImage} alt='news' />
               </div>
               <p>
                 {news.description > 100
